021d61c8d54233410ab647c185b1050a7d3502a2
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-01-04 13:56:29 -0600

    home page

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,30 @@
+
+<!-- Navigation start -->
+
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+	<div class="container">
+
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">NewCAN</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="/who">Who We Are</a></li>
+				<li><a href="/what">What We Do</a></li>
+				<li><a href="/join">Join Us</a></li>
+			</ul>
+		</div>
+
+	</div>
+</nav>
+
+<!-- Navigation end -->
