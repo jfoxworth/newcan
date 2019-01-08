3f95754e51e9bc13cefe13ebb371ad3d3cdb2b0c
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-01-08 17:02:05 -0600

    form tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class UserController extends Controller
 {
