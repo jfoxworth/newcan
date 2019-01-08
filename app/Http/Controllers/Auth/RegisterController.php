f68412ed621b46c1017b1c82c9993e83f0f279a9
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-01-08 14:20:08 -0600

    database linkup

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -63,6 +63,9 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        echo($data);
+
+        return;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
---
+++ app/Http/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+
+Route::post('/JoinUs', 'RegisterController@create');
+
+
+
+?>
