dafb50a5d0a9ee0155cb81de4d6ff9319cc28f92
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-06-18 22:21:44 -0500

    Initial changes

--- resources/views/landing/userBreak.blade.php
+++ resources/views/landing/userBreak.blade.php
@@ -3,7 +3,7 @@
 <a href="/JoinUs" class="button button-full button-dark center tright">
 
 	<div class="container clearfix">
-		Join Us as a candidate or a work to get <strong>someone elected</strong> <i class="icon-caret-right" style="top:4px;"></i>
+		Join Us to enforce immigration laws, end affirmative action, and restore US rights<i class="icon-caret-right" style="top:4px;"></i>
 
 	</div>
 
