05bada701a232eefcb3252b4e96e247e558dedee
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-06-19 07:16:47 -0500

    Initial changes

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,6 +1,17 @@
 
 <head>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142419440-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-142419440-1');
+	</script>
+
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
